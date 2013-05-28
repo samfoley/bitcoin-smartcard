3ef83214812ba3c71e48699acca63e1a0d51444a
Sam Foley <sam@samfoley.com>
2013-05-28 12:43:08 +1000

    ECDSA bug fixes

--- usb_gateway/test.c
+++ usb_gateway/test.c
@@ -15,6 +15,7 @@ typedef struct
 typedef struct 
 {
 	BIGNUM *b;
+	BIGNUM *p;
 } Curve;
 
 // Secp256k1 Parameters
@@ -24,6 +25,7 @@ const char G_hex[] = "0279BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B
 const char G1_hex[] = "0479BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8";
 const char Gx_hex[] = "79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798";
 const char Gy_hex[] = "483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8";
+const char Da_hex[] = "483ADA7726A344655DA4FBFC0E5508A8FD17B44BAD8554199C47D08FFB10D4B8";
 const char n_hex[] = "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141";
 int h = 1;
 BN_CTX *ctx = NULL;
@@ -53,9 +55,10 @@ int main()
 	digest[9]=17;
 	digest[12]=59;
 	digest[30]=87;
-	unsigned char priv_key_bin[37];	
+	unsigned char priv_key_bin[40];	
 	ECDSA_SIG *ecsig = NULL;
     EC_POINT *pub_key = NULL;
+	BIGNUM *tmp = BN_new();
 	
 	if ((ctx = BN_CTX_new()) == NULL)
 	{
@@ -64,7 +67,10 @@ int main()
 	}	
 	dA = BN_new();
 	_blkmk_b58tobin(priv_key_bin, sizeof(priv_key_bin), priv_key_b58, 0);
-	BN_bin2bn(priv_key_bin+1, 32, dA);
+	
+	BN_bin2bn(&priv_key_bin[1], 32, dA);
+	
+	BN_hex2bn(&dA, Da_hex);
 	
 	if (eckey == NULL)
     {
@@ -96,6 +102,12 @@ int main()
 		fprintf(stderr,"EC mul fail\n");
 		return -1;
 	}
+	
+	printf("QA: ");
+	//BN_print_fp(stdout, pub_key->x);	
+	EC_POINT_point2bn(group, pub_key, POINT_CONVERSION_UNCOMPRESSED, tmp, ctx);
+	BN_print_fp(stdout, tmp);
+	printf("\n");
 	
 	EC_KEY_set_private_key(eckey,dA);
     EC_KEY_set_public_key(eckey,pub_key);
@@ -159,8 +171,16 @@ ECDSA_SIG* ECDSA_test_sign(unsigned char *digest, int digest_len, unsigned char 
 	BN_hex2bn(&(G.y), Gy_hex);
 	BN_hex2bn(&n, n_hex);
 	mod = n;
-	Point *curve_point;
+	Point *curve_point, *point;
 	c.b = b;
+	c.p=p;
+	
+	point = ec_point_mul(&G, dA, &c);
+	printf("QA x: ");
+	BN_print_fp(stdout, point->x);	
+	printf("QA y: ");
+	BN_print_fp(stdout, point->y);	
+	printf("\n");
 	
 	// 2. 
 	BN_bin2bn(digest, digest_len, z);
@@ -180,7 +200,7 @@ ECDSA_SIG* ECDSA_test_sign(unsigned char *digest, int digest_len, unsigned char 
 	// 6. s = k^-1(z + rdA) (mod n)
 	BN_mod_mul(tmp, r, dA, mod, ctx);
 	BN_mod_add(tmp2, z, tmp, mod, ctx);
-	BN_mod_inverse(tmp, z, mod, ctx);
+	BN_mod_inverse(tmp, k, mod, ctx);
 	BN_mod_mul(s, tmp, tmp2, mod, ctx);
 	
 	printf("r: ");
@@ -199,6 +219,7 @@ Point* ec_point_mul(Point *P, BIGNUM *n, Curve *c)
 	BIGNUM *lambda = BN_new();
 	BIGNUM *tmp = BN_new();
 	BIGNUM *tmp2 = BN_new();
+	BIGNUM *tmp3 = BN_new();
 	
 	BIGNUM *xr = BN_new();
 	BIGNUM *yr = BN_new();
@@ -212,21 +233,23 @@ Point* ec_point_mul(Point *P, BIGNUM *n, Curve *c)
 			// f(2P, n/2)
 			
 			// lambda = (3x^2 + b)/2y
-			BN_mod_sqr(lambda, P->x, mod, ctx);			
-			BN_mul_word(lambda, 3);			
-			BN_mod_add(tmp, lambda, c->b, mod, ctx);
-			BN_rshift1(tmp,tmp);
-			BN_div(lambda, NULL, tmp, P->y, ctx);
+			BN_lshift1(tmp,P->y);		
+			BN_mod(tmp3, tmp, c->p, ctx);
+			BN_mod_sqr(tmp, P->x, c->p, ctx);			
+			BN_mul_word(tmp, 3);						
+			BN_mod_inverse(tmp2, tmp3, c->p, ctx);
+			BN_mod_mul(lambda, tmp, tmp2, c->p, ctx);
 			
 			// xr = lambda^2 - 2x
-			BN_mod_sqr(tmp, lambda, mod, ctx);
+			BN_mod_sqr(tmp, lambda, c->p, ctx);
 			BN_lshift1(tmp2, P->x);
-			BN_mod_sub(xr, tmp, tmp2, mod, ctx);
+			BN_mod(tmp3, tmp2, c->p, ctx);
+			BN_mod_sub(xr, tmp, tmp3, c->p, ctx);
 			
 			// yr = lambda(x - xr) - y
-			BN_mod_sub(tmp, P->x, xr, mod, ctx);
-			BN_mod_mul(tmp2, lambda, tmp, mod, ctx);
-			BN_mod_sub(yr, tmp2, P->y, mod, ctx);
+			BN_mod_sub(tmp, P->x, xr, c->p, ctx);
+			BN_mod_mul(tmp2, lambda, tmp, c->p, ctx);
+			BN_mod_sub(yr, tmp2, P->y, c->p, ctx);
 			
 			p2->x=xr;
 			p2->y=yr;
@@ -237,23 +260,25 @@ Point* ec_point_mul(Point *P, BIGNUM *n, Curve *c)
 			return ec_point_mul(p2, tmp, c);
 		} else {
 			// P + f(P, n-1)
-			BN_mod_sub(tmp, n, BN_value_one(), mod, ctx);
+			BN_mod_sub(tmp, n, BN_value_one(), c->p, ctx);
 			p2 = ec_point_mul(P, tmp, c);
 			
 			// lambda = (yq-yp)/(xq-xp)
-			BN_mod_sub(tmp, p2->y, P->y, mod, ctx);
-			BN_mod_sub(tmp2, p2->x, P->x, mod, ctx);
-			BN_div(lambda, NULL, tmp, tmp2, ctx);
+			BN_mod_sub(tmp, p2->y, P->y, c->p, ctx);
+			BN_mod_sub(tmp2, p2->x, P->x, c->p, ctx);
+			BN_mod_inverse(tmp3, tmp2, c->p, ctx);
+			BN_mod_mul(lambda, tmp, tmp3, c->p, ctx);
+			
 			
 			// xr = lambda^2 - xp - xq
-			BN_mod_sqr(tmp, lambda, mod, ctx);
-			BN_mod_sub(tmp2, tmp, P->x, mod, ctx);
-			BN_mod_sub(xr, tmp2, p2->x, mod, ctx);
+			BN_mod_sqr(tmp, lambda, c->p, ctx);
+			BN_mod_sub(tmp2, tmp, P->x, c->p, ctx);
+			BN_mod_sub(xr, tmp2, p2->x, c->p, ctx);
 			
 			// yr = lambda(xp - xr) - yp
-			BN_mod_sub(tmp, P->x, xr, mod, ctx);
-			BN_mod_mul(tmp2, lambda, tmp, mod, ctx);
-			BN_mod_sub(yr, tmp2, P->y, mod, ctx);
+			BN_mod_sub(tmp, P->x, xr, c->p, ctx);
+			BN_mod_mul(tmp2, lambda, tmp, c->p, ctx);
+			BN_mod_sub(yr, tmp2, P->y, c->p, ctx);
 			
 			// TODO FREE MEMORY
 			p2->x = xr;
